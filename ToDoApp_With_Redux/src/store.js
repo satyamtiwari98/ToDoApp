64c27e0f2b674a639f56351560462373abf8a7f6
Satyam Tiwari <satyamtiwari1598@gmail.com>
2024-01-13 00:00:58 +0530

    New TodoApp Added That is built using Redux

---
+++ ToDoApp_With_Redux/src/store.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { thunk } from "redux-thunk";
+import { TodoReducers } from "./reducers/TodoReducers";
+
+const reducer = combineReducers({
+  // this contains all reducers
+  Todo: TodoReducers,
+});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
